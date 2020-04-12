133e9f9f72d9b2becf78c670569bc4148ddf36dc
valoeghese <valoeghesemods@gmail.com>
2020-04-13 01:12:05 +1200

    base

---
+++ src/main/java/tk/valoeghese/shuttle/Shuttle.java
@@ -0,0 +1,10 @@
+package tk.valoeghese.shuttle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Shuttle implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello from Shuttle!");
+	}
+}
