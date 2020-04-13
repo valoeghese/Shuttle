26d628a0943464dc1b831e05424253671dccfdf6
valoeghese <valoeghesemods@gmail.com>
2020-04-13 13:20:20 +1200

    javadoc Player

--- src/main/java/tk/valoeghese/shuttle/api/player/Player.java
+++ src/main/java/tk/valoeghese/shuttle/api/player/Player.java
@@ -2,9 +2,24 @@ package tk.valoeghese.shuttle.api.player;
 
 import java.util.UUID;
 
+/**
+ * Interface representing a player on the server, which provides utilities to interact with the said player.
+ */
 public interface Player {
+	/**
+	 * @return the username of the player.
+	 */
 	String getUsername();
+	/**
+	 * @return the unique identifier of the player.
+	 */
 	UUID getUUID();
+	/**
+	 * Sends the specified chat message to the player.
+	 */
 	void sendMessage(String message);
+	/**
+	 * Sends the specified chat message to the player.
+	 */
 	void sendMessage(ChatMessage message);
 }
