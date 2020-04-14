c1b2753abaed2b1b384cf2e833c622634486a18a
valoeghese <valoeghesemods@gmail.com>
2020-04-14 23:49:36 +1200

    hmmm

--- src/main/java/tk/valoeghese/shuttle/api/player/Inventory.java
+++ src/main/java/tk/valoeghese/shuttle/api/player/Inventory.java
@@ -1,7 +1,10 @@
 package tk.valoeghese.shuttle.api.player;
 
+import net.minecraft.item.ItemStack;
+
 public interface Inventory {
 	/**
+	 * @param index the index of the inventory.
 	 * @return the stack at the given index.
 	 */
 	ItemStack getStack(int index);
