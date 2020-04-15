3f20a419aeb95d73ae644d9d27148ef9628f8d4e
valoeghese <valoeghesemods@gmail.com>
2020-04-15 13:29:41 +1200

    direct brigadier exposure option

--- src/main/java/tk/valoeghese/shuttle/api/server/SetupEvents.java
+++ src/main/java/tk/valoeghese/shuttle/api/server/SetupEvents.java
@@ -1,5 +1,11 @@
 package tk.valoeghese.shuttle.api.server;
 
+import java.util.function.Consumer;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+import net.fabricmc.fabric.api.registry.CommandRegistry;
+import net.minecraft.server.command.ServerCommandSource;
 import tk.valoeghese.shuttle.api.command.Command;
 import tk.valoeghese.shuttle.api.event.Context;
 import tk.valoeghese.shuttle.api.event.ShuttleEventListener;
@@ -18,7 +24,7 @@ public final class SetupEvents {
 	 */
 	public static interface ShuttleCommandSetup extends ShuttleEventListener {
 		/**
-		 * Called at command setup.
+		 * Called at command setup, on the launch of the game.
 		 */
 		void setupCommands(CommandSetupContext context);
 	}
@@ -27,12 +33,27 @@ public final class SetupEvents {
 	 * Context providing an interface to register commands.
 	 */
 	public static class CommandSetupContext implements Context<ShuttleCommandSetup> {
+		/**
+		 * Registers a command built with shuttle's command api.
+		 * 
+		 * @param command the shuttle command instance
+		 */
 		public void registerCommand(Command command) {
 			if (command.isSubCommand()) {
 				throw new RuntimeException("Cannot directly register a sub command! Sub commands are registered when the main command is registered.");
 			}
 
 			BrigadierCommandBuilder.buildAndRegister(command);
+		}
+
+		/**
+		 * Registers a command built directly with the brigadier library to the game.
+		 * 
+		 * @param onlyOnDedicated if true, the command is only registered on a dedicated (i.e. not singleplayer integrated) server.
+		 * @param commandSetup consumer which sets up the command from the supplied command dispatcher.
+		 */
+		public void registerBrigadierCommand(boolean onlyOnDedicated, Consumer<CommandDispatcher<ServerCommandSource>> commandSetup) {
+			CommandRegistry.INSTANCE.register(false, commandSetup);
 		}
 	}
 }
